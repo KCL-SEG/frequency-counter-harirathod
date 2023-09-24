03245c7ebb50b9df16df0782d920219c3e77f835
Hari Rathod <hari.rathod@kcl.ac.uk>
2023-09-24 16:34:52 +0100

    Implement frequencies function

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,17 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def frequencies(items):
+def frequencies(items: list) -> dict: 
     frequencies = {}
-    # Your code goes here
+    # Take a list and return a dictionary with key = item in items and value = frequency
+    # For each item in items, if the item is in the dictionary, add one to that item with d[str(item)] += 1
+    # Otherwise, do frequencies[str(item)] = 0
+    for item in items:
+        if str(item) in frequencies.keys():
+            frequencies[str(item)] += 1
+        else:
+            frequencies[str(item)] = 1
     return frequencies
+
+if __name__ == "__main__":
+    print(frequencies([100,'100',100,"100",'a']))
